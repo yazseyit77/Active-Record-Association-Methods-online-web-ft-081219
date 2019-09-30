99880e6a989c1fe99e09b2067ba94fe55998f3ec
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-30 20:15:41 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,6 +4,9 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    def get_first_song
+      self.songs
+    end
   end
 
   def song_count
